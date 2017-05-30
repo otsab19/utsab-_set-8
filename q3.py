83229a75ada332f3281f3394e5074bac9fd77df1
otsab <chaliseutsab@gmail.com>
2017-05-30 08:58:59 +0545

    new

---
+++ q3.py
@@ -0,0 +1,19 @@
+Quality assurance :
+-- monitors production operations and helps to improve the manufacturing 
+process and prevent defects in the finished product.
+--A car company assurance of its product is resilient on manufacturing process to produce the 
+	required product.
+--everyone of the team is responsible for QA.
+--verification on every day of manufacturing.
+
+
+
+
+
+Quality Control
+--ensures all products meet company standards before entering the market.
+--A car company tests its each and every parts and gears to finalise the car it is producing is 
+	working efficiently as per the goal .
+--A separate team is assigned for testing for QC.
+
+--Validation and testing before the product is released.
